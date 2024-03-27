acbc0d0305e3db858bcdfeae4e9b5036dc5ffa8d
Jess Liu <jessica040532@gmail.com>
2024-03-27 20:06:41 +1300

    Get posts and add new post apis

--- src/server.js
+++ src/server.js
@@ -11,6 +11,37 @@ const db = getFirestore();
 const app = express();
 app.use(express.json());
 
+app.get('/api/posts', async (req, res) => {
+  try {
+    const postsRef = db.collection('posts');
+    const snapshot = await postsRef.get();
+    let responseArr = []
+  
+    snapshot.forEach(doc => {
+      responseArr.push(doc.data())
+    })
+    res.send(responseArr)
+  } catch(error) {
+    res.send(error)
+  }
+})
+
+app.post('/api/posts/add', async (req, res) => {
+  try {
+    const docRef = await db.collection('posts').add({
+      ...req.body,
+      id: null
+    })
+    // add generated id into data
+    await docRef.update({ id: docRef.id });
+
+    res.json({id: docRef.id, ...req.body})
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500).json({error: "Faild to add new post"})
+  }
+})
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
